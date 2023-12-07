41720a209b0a5ae4d337ccc79c8e8ac4bc1fc83c
DAVID SANCHEZ <72284986+David-Sanchez-Sangay@users.noreply.github.com>
2023-12-06 20:46:09 -0500

    build asdasdas

--- resources/js/Components/Nav.jsx
+++ resources/js/Components/Nav.jsx
@@ -32,7 +32,7 @@ const Nav = () => {
             <nav>
                 <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
                     <div className="flex items-center flex-1">
-                        <span className="text-3xl font-bold">INSIGNIA DEL COLEGIO</span>
+                        <span className="text-3xl font-bold">ANTONIO RAIMONDI</span>
                     </div>
                     <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
                         <div className="flex-10">
