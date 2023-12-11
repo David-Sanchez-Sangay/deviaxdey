281ae0bbde6bad2f2211d6ff5227b7ec9fbe74da
DAVID SANCHEZ <72284986+David-Sanchez-Sangay@users.noreply.github.com>
2023-12-11 15:44:47 -0500

    frontend the mesadepartes
    quest

--- resources/js/Components/MesaPartes.jsx
+++ resources/js/Components/MesaPartes.jsx
@@ -8,14 +8,13 @@ const MesaPartes = () => {
   };
 
   const handleValidation = () => {
-    // Aquí puedes agregar la lógica de validación del correo según tus necesidades
-    // Por ahora, simplemente imprimiré el correo en la consola
+
     console.log('Correo validado:', email);
   };
 
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="bg-sky-300 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="flex justify-center relative mt-20">
+      <div className="bg-cyan-200 shadow-md box-border rounded px-8 pt-6 pb-8 mb-4">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailInput">
           Correo Electrónico:
         </label>
@@ -27,7 +26,7 @@ const MesaPartes = () => {
           onChange={handleEmailChange}
         />
         <button
-          className="bg-blue-500 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded mt-4"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
           onClick={handleValidation}
         >
           Validar
