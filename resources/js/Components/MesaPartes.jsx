1e36b8c05a4eb7f078d84329c0c72e19f76385b8
Daniel <jesus.cs.7.2010@gmail.com>
2023-12-11 23:44:17 -0500

    Revert "Merge branch 'main' of https://github.com/David-Sanchez-Sangay/deviaxdey"
    This reverts commit d2e7ebc3548a45721775ec9ff6611473039c95ff, reversing
    changes made to 06b1a61eb354147d1d24eeb69415d306888d8127.

--- resources/js/Components/MesaPartes.jsx
+++ resources/js/Components/MesaPartes.jsx
@@ -1,39 +1,9 @@
-import React, { useState } from 'react';
-
-const MesaPartes = () => {
-  const [email, setEmail] = useState('');
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleValidation = () => {
-
-    console.log('Correo validado:', email);
-  };
-
-  return (
-    <div className="flex justify-center relative mt-20">
-      <div className="bg-sky-300 shadow-md box-border rounded px-8 pt-6 pb-8 mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="emailInput">
-          Correo Electrónico:
-        </label>
-        <input
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="emailInput"
-          type="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-          onClick={handleValidation}
-        >
-          Validar
-        </button>
-      </div>
-    </div>
-  );
+const MesaPartes = () =>{
+    return (
+        <div>
+            <h1>MesaPartes</h1>
+        </div>
+    );
 };
 
 export default MesaPartes;
