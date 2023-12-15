0eb07642dcab3cd8354ee0a480054464d731b8fa
Daniel <jesus.cs.7.2010@gmail.com>
2023-12-14 20:12:38 -0500

    Intento v3

--- resources/js/api.js
+++ resources/js/api.js
@@ -6,7 +6,7 @@ const DB_DATABASE="deviaxde_raimondi_db"
 const DB_USERNAME="deviaxde_david2"
 const DB_PASSWORD="A%B0rZ3S(~Gn"
 
-const baseUrl = 'https://deviaxdey.com/api'
+const baseUrl = 'https://deviaxdey.com/public/api'
 
 
 export async function getTeachers () {
